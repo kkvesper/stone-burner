ef458a2b599a4aae0432d1151e031a5bf11632e0
Rodrigo Argüello Flores <rarguellof91@gmail.com>
2018-01-17 00:37:24 +0100

    Fix setup.py encoding

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='stone-burner',
-    version='0.7.0',
+    version='0.7.1',
     description='Give more power to Terraform.',
     author='Rodrigo Argüello Flores',
     author_email='rodrigo@kkvesper.jp',
     url='https://github.com/kkvesper/stone-burner',
-    download_url='https://github.com/kkvesper/stone-burner/archive/v0.7.0.tar.gz',
+    download_url='https://github.com/kkvesper/stone-burner/archive/v0.7.1.tar.gz',
     packages=find_packages(),
     install_requires=[
         'jinja2>=2.9',
